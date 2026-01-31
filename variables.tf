fc13fa9775e12c86a51564354b0389e637d7936a
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2026-01-31 17:26:38 +0900

    add:VPCの作成

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "workspace_name" {
+  description = "Name of the source workspace to query."
+  type        = string
+  default     = "sbcntr-terraform"
+}
+
+variable "organization_name" {
+  description = "Name of the HCP Terraform organization with the source workspace"
+  type        = string
+  default = "takaoyamasaki1101"
+}
